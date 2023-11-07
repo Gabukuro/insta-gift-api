2ed7bc8c7756507a6823f409365ba8211708e7fe
Gabukuro <gabriel.machado@transfeera.com>
2023-11-07 09:30:35 -0300

    fix: lint warning

--- cmd/main.go
+++ cmd/main.go
@@ -30,7 +30,7 @@ func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	go func() {
-		_ = <-c
+		<-c
 		fmt.Println("Gracefully shutting down...")
 		routerInstance.Stop()
 	}()
